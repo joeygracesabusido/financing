2cb8b811311557127a56eaacf3c7efaeb2f52e5c
unknown <joeysabusido.@gmail.com>
2023-05-21 22:32:48 +0300

    05.21.2023 10:32 PM

--- config/users.py
+++ config/users.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from pydantic import condecimal
-from sqlmodel import Field, Session, SQLModel, create_engine,select,func,funcfilter,within_group
+from sqlmodel import Field, Session, SQLModel, create_engine,select,func,funcfilter,within_group,Relationship
 
 from datetime import datetime, date
 
@@ -28,7 +28,8 @@ roles = [{
     "admin": 1,
     "manager": 2,
     "supervisor": 3,
-    "user": 4
+    "rank_file": 4,
+    "user": 5
 }]
 
 # Role List and Function
@@ -36,7 +37,12 @@ roles = [{
     #         can access basic Function CRUD 
     # manager = can do basic function (CRUD) but cannot access to database
 
-
+class role(SQLModel, table=True):
+    """This is to create role"""
+    id: Optional[int] = Field(default=None, primary_key=True)
+    roles: str = Field(index=True, default=None)
+    date_updated: datetime = Field(default=None)
+    date_credited: datetime
 
 class user(SQLModel, table=True):
     """This is to create user Table"""
@@ -44,7 +50,22 @@ class user(SQLModel, table=True):
     username: str = Field(index=True)
     password: str = Field(default=None)
     email_add: str = Field(default=None)
-    role: str = Field(default=None)
+    role_id:  Optional[int] = Field(foreign_key="role.id")
+    date_updated: Optional[datetime] = Field(default=None)
+    date_credited: Optional[datetime] = Field(default=datetime.now())
+
+class account(SQLModel, table=True):
+    """This is account info for Clients"""
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+class userAccountTagging(SQLModel, table=True):
+    """This is for tag for user account"""  
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user_id:  Optional[int] = Field(foreign_key="user.id")
+    account_id: Optional[int] = Field(foreign_key="account.id")
+
+class employee(SQLModel, table=True):
+    """"""
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
